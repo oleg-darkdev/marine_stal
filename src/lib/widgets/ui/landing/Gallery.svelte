71becb9021af3ed065a2f34cae4c69c1c8b2c3da
DarkDev <darkdev_brand@riseup.net>
2023-06-12 17:59:13 +0300

    upd full path for images

--- src/lib/widgets/ui/landing/Gallery.svelte
+++ src/lib/widgets/ui/landing/Gallery.svelte
@@ -1,9 +1,9 @@
 <script>
 	const gallery = [
-		['1.jpg', '2.jpg', '3.jpg'],
-		['4.jpg', '5.jpg', '6.jpg'],
-		['7.jpg', '8.jpg', '9.jpg'],
-		['10.jpg', '11.jpg', '12.jpg']
+		['/images/gallery/1.jpg', '/images/gallery/2.jpg', '/images/gallery/3.jpg'],
+		['/images/gallery/4.jpg', '/images/gallery/5.jpg', '/images/gallery/6.jpg'],
+		['/images/gallery/7.jpg', '/images/gallery/8.jpg', '/images/gallery/9.jpg'],
+		['/images/gallery/10.jpg', '/images/gallery/11.jpg', '/images/gallery/12.jpg']
 	];
 </script>
 
@@ -12,21 +12,21 @@
 		<div>
 			<img
 				class="blur-xs hover:contrast-none h-auto max-w-full transform  rounded-lg  grayscale filter transition delay-150 duration-300 ease-in-out  hover:-translate-y-8 hover:blur-none hover:filter-none"
-				src="/images/gallery/{imgCollection[0]}"
+				src={imgCollection[0]}
 				alt=""
 			/>
 		</div>
 		<div>
 			<img
 				class="blur-xs hover:contrast-none h-auto max-w-full transform  rounded-lg  grayscale filter transition delay-150 duration-300 ease-in-out  hover:-translate-y-8 hover:blur-none hover:filter-none"
-				src="/images/gallery/{imgCollection[1]}"
+				src={imgCollection[1]}
 				alt=""
 			/>
 		</div>
 		<div>
 			<img
 				class="blur-xs hover:contrast-none h-auto max-w-full transform  rounded-lg  grayscale filter transition delay-150 duration-300 ease-in-out  hover:-translate-y-8 hover:blur-none hover:filter-none"
-				src="/images/gallery/{imgCollection[3]}"
+				src={imgCollection[3]}
 				alt=""
 			/>
 		</div>
