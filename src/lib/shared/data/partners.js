b0ea80f503ac246f69dca4fa434deab20ef4cda7
DarkDev <darkdev_brand@riseup.net>
2023-06-12 17:38:04 +0300

    alfa v. data

---
+++ src/lib/shared/data/partners.js
@@ -0,0 +1,35 @@
+const partners = [
+	{
+		title: 'Remontowa River Shipyard Sp. z o.o.',
+		link: 'http://www.rrs.rh.pl/pl/',
+		logo: 'rrs.png'
+	},
+	{
+		title: 'Marine Projects Ltd.',
+		link: 'https://marineprojects.pl/',
+		logo: 'marine_progects.png'
+	},
+	{
+		title: 'Euro-Industry "Stocznia Ustka"',
+		link: 'https://stocznia-ustka.pl/',
+		logo: 'euro_industry.png'
+	},
+	{
+		title: 'Techno Marine Ltd.',
+		link: 'https://technomarine.pl/en/',
+		logo: 'techno_marine.png'
+	},
+	{
+		title: 'J-Marine',
+		link: 'https://j-marine.com.pl/en/offer',
+		logo: 'j-marine.png'
+	},
+	{
+		title: 'Warire Tech',
+		link: '',
+		logo: 'warire_tech.png'
+	}
+];
+
+
+export default partners;
