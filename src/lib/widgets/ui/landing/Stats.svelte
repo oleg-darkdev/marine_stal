0dd93debbeaf6f878edecd28ee08438a543d65a0
DarkDev <darkdev_brand@riseup.net>
2023-06-13 17:54:59 +0300

    add numbers counter.js for stats

--- src/lib/widgets/ui/landing/Stats.svelte
+++ src/lib/widgets/ui/landing/Stats.svelte
@@ -1,44 +1,41 @@
 <script>
+	import { CountUp } from 'countup.js';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const workersCountUp = new CountUp('workers', 50, { enableScrollSpy: true, duration: 6 }),
+			hoursCountUp = new CountUp('hours', 900000, { enableScrollSpy: true, duration: 8 }),
+			partnersCountUp = new CountUp('partners', 12, { enableScrollSpy: true, duration: 6 }),
+			// progectsCountUp = new CountUp('progects', 9, { enableScrollSpy: true, duration: 8 }),
+			experienceCountUp = new CountUp('experience', 10, { enableScrollSpy: true, duration: 13 });
+	});
+
 	const stats = [
 		{
-			title: 'Lorem impsum',
-			text: '50+',
-			id: 1
+			title: 'Lat na rynku',
+			id: 'experience'
 		},
 		{
-			title: 'Lorem impsum',
-			text: '90000',
-			id: 2
+			title: 'Pracowników w firmie',
+			id: 'workers'
 		},
 		{
-			title: 'Lorem impsum',
-			text: '130+',
-			id: 3
+			title: 'Firmy partnerskie',
+			id: 'partners'
 		},
 		{
-			title: 'Lorem impsum',
-			text: '12',
-			id: 4
-		},
-		{
-			title: 'Lorem impsum',
-			text: '7',
-			id: 5
-		},
-		{
-			title: 'Lorem impsum',
-			text: '8+',
-			id: 6
+			title: 'Czas pracy na projektach',
+			id: 'hours'
 		}
 	];
 </script>
 
 <div
-	class=" container mx-auto grid  max-w-4xl grid-cols-2 justify-center gap-4 text-center lg:grid-cols-3"
+	class=" container mx-auto grid  max-w-4xl grid-cols-2 justify-center text-center lg:grid-cols-4"
 >
 	{#each stats as stat}
-		<div class="m-2 flex flex-col justify-start">
-			<p class="text-4xl font-bold leading-none text-white lg:text-8xl">{stat.text}</p>
+		<div class=" flex w-3/12 flex-col justify-start">
+			<p id={stat.id} class="text-4xl font-bold leading-none text-white lg:text-8xl" />
 			<p class="text-md sm:text-base">{stat.title}</p>
 		</div>
 	{/each}
