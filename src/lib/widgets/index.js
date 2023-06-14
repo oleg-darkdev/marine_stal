206d7f69e1a2cb284b95d78f85b27faac4c96096
DarkDev <darkdev_brand@riseup.net>
2023-06-14 17:12:50 +0300

    add Contact section

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -8,9 +8,11 @@ import Stats from './ui/landing/Stats.svelte';
 import Gallery from './ui/landing/Gallery.svelte';
 import BreadCrumbs from './ui/landing/BreadCrumbs.svelte';
 import ProgectGallery from './ui/landing/ProgectGallery.svelte';
+import Contact from './ui/landing/Contact.svelte';
 
 export {
 	Header,
+  Contact,
 	About,
 	Partners,
 	Slider,
---
+++ src/lib/widgets/ui/landing/Contact.svelte
@@ -0,0 +1,39 @@
+<section class="-mt-20 mb-32 text-center">
+	<div class=" md:px-12">
+		<div class="container mx-auto xl:px-32">
+			<div class="grid items-center lg:grid-cols-2">
+				<div class="mb-12 md:mt-12 lg:mt-0 lg:mb-0">
+					<div
+						class="relative z-[1] block max-w-2xl rounded-lg  bg-[hsla(0,0%,5%,0.7)] px-6  py-12 shadow-black/20 backdrop-blur-[30px] md:px-12 lg:-mr-14"
+					>
+						<!-- <div
+								class="mb-10 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:px-6"
+							> -->
+						<h2 class="mb-6 text-3xl font-bold">Contact us</h2>
+						<p class="mb-6 text-neutral-500 dark:text-neutral-300">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, modi accusantium
+							ipsum corporis quia asperiores dolorem nisi corrupti eveniet dolores ad maiores
+							repellendus enim autem omnis fugiat perspiciatis? Ad, veritatis.
+						</p>
+						<p class="mb-2 text-neutral-500 dark:text-neutral-300">
+							New York, 94126, United States
+						</p>
+						<p class="mb-2 text-neutral-500 dark:text-neutral-300">+ 01 234 567 89</p>
+						<p class="mb-2 text-neutral-500 dark:text-neutral-300">info@gmail.com</p>
+					</div>
+					<!-- </div> -->
+				</div>
+				<div class="md:mb-12 lg:mb-0">
+					<div class="relative h-[700px] rounded-lg shadow-lg dark:shadow-black/20">
+						<iframe
+							src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed"
+							class="absolute left-0 top-0 h-full w-full rounded-lg"
+							frameborder="0"
+							allowfullscreen
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
